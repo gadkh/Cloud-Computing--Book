080a2e7497bb32961d9cf9a26237e68fdeeec7e3
Gadkh2 <gadkh2@gmail.com>
2019-04-07 12:24:48 +0300

    continuing book class 7.4.2019

--- src/main/java/Lib/BookLogic.java
+++ src/main/java/Lib/BookLogic.java
@@ -1,5 +1,9 @@
 package Lib;
 
+import java.util.Optional;
+
 public interface BookLogic {
 	public Book CreateBook(Book book);
+	public Optional<Book> getBookByISBN(String ISBN);
+	public void removeAllBooks();
 }
--- src/main/java/Lib/BookLogicImpl.java
+++ src/main/java/Lib/BookLogicImpl.java
@@ -1,11 +1,38 @@
 package Lib;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class BookLogicImpl implements BookLogic{
+	private BookDao bookDao;
+//	private PublisherDao publisherDao;
+	
+	public BookLogicImpl(BookDao bookDao)
+	{
+		this.bookDao=bookDao;
+//		this.publisherDao=publisherDao;
+	}
+	
+	@Override
+	@Transactional
+	public Book CreateBook(Book book) {
+//		this.publisherDao.save(book.getPublisher());
+		return this.bookDao.save(book);
+	}
 
 	@Override
-	public Book CreateBook(Book book) {
-		// TODO Auto-generated method stub
-		return null;
+	@Transactional(readOnly=true)
+	public Optional<Book> getBookByISBN(String ISBN) {
+		return this.bookDao.findById(ISBN);
+	}
+
+	@Override
+	@Transactional
+	public void removeAllBooks() {		
+		this.bookDao.deleteAll();
 	}
 
 }
